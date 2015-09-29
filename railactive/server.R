ea66dc3f956fc6139249a939cb6283aceeb4d200
Dima Gorenshteyn <dima@Dimas-MacBook-Pro.local>
2015-09-29 18:51:19 -0400

    General cleanup/annotation

--- railactive/server.R
+++ railactive/server.R
@@ -1,4 +1,3 @@
-# TODO: Merge the repos into a single R repository
 # TODO: Build fault tolerance into app (i.e. maximum wait time & caching for demo)
 library(shiny)
 library(shinydashboard)
@@ -12,14 +11,21 @@ library(caret)
 library(randomForest)
 
 shinyServer(function(input, output, session) {
-  main_info <- read.csv("main_info.csv")
-  load(file = "modelfit.rda")
-  load(file = "cpt_complete.rda")
-  #A parameter of the model (time to delay is constrained to a max of 24 hours)
+
+  # Load required data --------
+  main_info <- readRDS(file = "data/main_info.rds")
+  modelfit <- readRDS(file = "data/modelfit.rds")
+  # Contains the breaks necessary to build conditional probiability tables for live data
+  load(file = "data/cpt_complete.rda")
+
+  # Global options --------------
+  #A parameter of the model (time to last delay is constrained to a max of 24 hours)
   max_delay = 24*60
 
-
+  # Reactive/Query Functions -------------
+  # Current Weather in Newark Airport (Proxy for NJ weather)
   getCurrentWeather <- reactive({
+    # TODO: Specify timeout & handle error
     hourly_newark <- "http://forecast.weather.gov/MapClick.php?lat=40.7242&lon=-74.1726&FcstType=json"
     w_get <- GET(hourly_newark)
     w_text <- content(w_get, as = "text") %>% fromJSON()
@@ -27,46 +33,14 @@ shinyServer(function(input, output, session) {
       "Visibility" = as.double(w_text$currentobservation$Visibility[1]),
       "WindSpeed" = as.numeric(w_text$currentobservation$Winds[1]))
   })
+
+  # Query ttld service for list of previous delays
   getTTLD <- reactive({
-    read.csv("http://52.88.4.39/ttld_log.txt")
+    read.csv("http://52.88.4.39/ttld_log.txt") # Hosted on aws with elastic ip
   })
 
-  buildCurrentPredictionDF <- reactive({
-    input$refresh
-    interval <- max(as.numeric(input$interval), 5)
-    if(input$interval != 0) invalidateLater(interval * 1000, session)
-
-
-    current_weather <- getCurrentWeather()
-    ttld_file <- getTTLD()
-
-    # Process Date/Time
-    pred_df <- main_info %>% mutate(Current_Datetime = format(Sys.time(), tz = "America/New_York"), #make sure system time is correct on server
-                                    dep_hour = hour(Current_Datetime),
-                                    dep_mon = month(Current_Datetime, label = TRUE),
-                                    dep_wday = wday(Current_Datetime, label = TRUE))
-
-    # Retriev & Process previous delays that were more than 10 minutes long
-
-    pred_df <- ttld_file %>%
-      select(ttld_Line = Line, Scheduled_Start_Time) %>%
-      right_join(pred_df) %>%
-      mutate(ttl_line = as.numeric(difftime(Current_Datetime, Scheduled_Start_Time, units = "mins"))) %>%
-      mutate(ttl_line = ifelse(is.na(ttl_line)|ttl_line > max_delay, max_delay, ttl_line))
-
-    # Obtain current weather for Newark
-
-    pred_df$Temp_F <- current_weather["Temp_F"]
-    pred_df$Visibility <- current_weather["Visibility"]
-    pred_df$WindSpeed <- current_weather["WindSpeed"]
-    return(pred_df)
-  })
-
+  # Build a weather dataframe for the next 12 hours
   buildHourlyWeatherDF <- reactive({
-    input$refresh
-    interval <- max(as.numeric(input$interval), 5)
-    if(input$interval != 0) invalidateLater(interval * 1000, session)
-
     current_weather <- getCurrentWeather()
     ttld_file <- getTTLD()
 
@@ -76,43 +50,97 @@ shinyServer(function(input, output, session) {
     w_text_hourly$hourly_forecast %>% flatten %>%
       transmute(WindSpeed = as.numeric(wspd.english),
                 Temp_F = as.numeric(temp.english),
+                # Fix date_time field for use as POSIXct
                 Date_Time = paste(FCTTIME.hour_padded,
                                   FCTTIME.min,
                                   FCTTIME.mon_padded,
                                   FCTTIME.mday_padded,
                                   FCTTIME.year)) %>%
       mutate(Date_Time = as.POSIXct(strptime(Date_Time, format = "%H %M %m %d %Y")),
-             Visibility = current_weather["Visibility"],
-             dep_hour = hour(Date_Time),
-             dep_mon = month(Date_Time, label = TRUE),
-             dep_wday = wday(Date_Time, label = TRUE)) %>%
-      slice(1:12)
+             # Visibility is not provided hourly, using current visibility as feature
+             Visibility = current_weather["Visibility"]) %>%
+      slice(1:12) #next 12 Hours
   })
 
+  # Build a dataframe to use for CPT and prediction functions for the current prediction
+  buildCurrentPredictionDF <- reactive({
+
+    # Refresh based on control panel
+    input$refresh
+    interval <- max(as.numeric(input$interval), 5)
+    if(input$interval != 0) invalidateLater(interval * 1000, session)
+
+    current_weather <- getCurrentWeather()
+    ttld_file <- getTTLD()
+
+    # Build Date/Time features
+    pred_df <- main_info %>%
+      mutate(Current_Datetime = format(Sys.time(), tz = "America/New_York"), #make sure system time is correct on server
+             dep_hour = hour(Current_Datetime),
+             dep_mon = month(Current_Datetime, label = TRUE),
+             dep_wday = wday(Current_Datetime, label = TRUE))
+
+    # Time to last delay feature
+    pred_df <- ttld_file %>%
+      select(ttld_Line = Line, Scheduled_Start_Time) %>%
+      right_join(pred_df) %>%
+      mutate(ttl_line = as.numeric(difftime(Current_Datetime, Scheduled_Start_Time, units = "mins"))) %>%
+      #Set time to last delay at max value when the value is exceeded or no previous delay has been recorded
+      mutate(ttl_line = ifelse(is.na(ttl_line)|ttl_line > max_delay, max_delay, ttl_line))
+
+    # Weather features
+    pred_df$Temp_F <- current_weather["Temp_F"]
+    pred_df$Visibility <- current_weather["Visibility"]
+    pred_df$WindSpeed <- current_weather["WindSpeed"]
+    return(pred_df)
+  })
+
+  # Build a dataframe to use for CPT and prediction functions for the 12-hour forecast
   buildHourlyPredictionDF <- reactive({
+
+    # Refresh based on control panel
+    input$refresh
+    interval <- max(as.numeric(input$interval), 5)
+    if(input$interval != 0) invalidateLater(interval * 1000, session)
+
+    # Weather Features
     hourly_weather <- buildHourlyWeatherDF()
     ttld_file <- getTTLD()
-    pred_df <- hourly_weather %>% mutate(Line = input$trainline) %>% inner_join(main_info, by = "Line")
-    ttld_file %>%
+
+    # date/time features
+    pred_df <- hourly_weather %>%
+      mutate(Line = input$trainline) %>%
+      inner_join(main_info, by = "Line") %>%
+      mutate(dep_hour = hour(Date_Time),
+             dep_mon = month(Date_Time, label = TRUE),
+             dep_wday = wday(Date_Time, label = TRUE))
+
+    # Time to last delay feature
+    pred_df <- ttld_file %>%
       select(ttld_Line = Line, Scheduled_Start_Time) %>%
       right_join(pred_df, by = "ttld_Line") %>%
       mutate(ttl_line = as.numeric(difftime(Date_Time, Scheduled_Start_Time, units = "mins"))) %>%
+      #Set time to last delay at max value when the value is exceeded or no previous delay has been recorded
       mutate(ttl_line = ifelse(is.na(ttl_line)|ttl_line > max_delay, max_delay, ttl_line))
+
+    return(pred_df)
   })
 
+  # Use model to output current probability of delay for each line
   predictCurrentProbabilities <- reactive({
     pred_df <- buildCurrentPredictionDF()
     if(!is.null(pred_df)){
       pred_df %>%
-      mutate(Line_Name = Full.Name) %>%
-      group_by(Line_Name, Line, dep_hour)  %>%
-      do(data.frame(p=predict(modelfit, ., type = "prob")[[1]][[1]][1])) %>%
-      ungroup %>%
+        mutate(Line_Name = Full.Name) %>%
+        group_by(Line_Name, Line, dep_hour)  %>%
+        do(data.frame(p=predict(modelfit, ., type = "prob")[[1]][[1]][1])) %>%
+        ungroup %>%
         mutate(pct = as.integer(round(p*100)),
-             txt = ifelse(pct<50, "low",ifelse(pct<75,"medium", "high")))
+               txt = ifelse(pct<50, "low",ifelse(pct<75,"medium", "high")))
     }else{NULL}
   })
 
+  # Use model to output 12-hour forecast of delay for each line
   predictHourlyProbabilities <- reactive({
     pred_df <- buildHourlyPredictionDF()
     if(!is.null(pred_df)){
@@ -125,7 +153,7 @@ shinyServer(function(input, output, session) {
     }else{NULL}
   })
 
-
+  # Build the conditional probability tables to explain the prediction
   buildCurrentCPT <- reactive({
     pred_df <- buildCurrentPredictionDF()
     if(!is.null(pred_df)){
@@ -133,13 +161,14 @@ shinyServer(function(input, output, session) {
         ttl_line_break = cut(ttl_line, breaks_ttl_line , include.lowest = TRUE),
         Temp_F_break = cut(Temp_F, breaks_Temp_F , include.lowest = TRUE),
         WindSpeed_break = cut(WindSpeed, breaks_WindSpeed , include.lowest = TRUE),
-        Visibility_break = cut(Visibility, breaks_Visibility , include.lowest = TRUE)
-      ) %>%
-        select(Line, dep_hour:Visibility_break) %>% gather(feature, feature_val, -Line, -ttl_line:-WindSpeed) %>%
+        Visibility_break = cut(Visibility, breaks_Visibility , include.lowest = TRUE)) %>%
+        select(Line, dep_hour:Visibility_break) %>%
+        gather(feature, feature_val, -Line, -ttl_line:-WindSpeed) %>%
         inner_join(cpt_complete)
     }else{NULL}
   })
 
+  # Appearance Functions -----------
   getStatusColor <- function(value){
     if(value < 50){
       "green"
@@ -148,12 +177,16 @@ shinyServer(function(input, output, session) {
     } else {"red"}
   }
 
-  # Header ---
+  # Header Output -------------------
 
+  # Train notifications
   output$notifications <- renderMenu({
     current_prob <- predictCurrentProbabilities()
+
     if(!is.null(current_prob)){
       current_prob <- current_prob %>% ungroup %>% arrange(-pct)
+
+      # Add message content based on current probabilities
       messages <- current_prob %>% count(txt)  %>%
         mutate(icontype = ifelse(txt == "low",
                                  "check" ,
@@ -165,6 +198,8 @@ shinyServer(function(input, output, session) {
       badge_status <- if(max_chance < 50) {"success"
       }else if(max_chance <75) {"warning"
       }else {"danger"}
+
+      # Build the messages as a list
       progress_msgs <- apply(messages, 1, function(row) {
         notificationItem(text = row[["message_txt"]],
                          icon = icon(row[["icontype"]])
@@ -172,17 +207,23 @@ shinyServer(function(input, output, session) {
       })
     }
     else progress_msgs = list()
+
     dropdownMenu(type = "notification", .list = progress_msgs, badgeStatus = badge_status)
   })
 
+  # Train status bars
   output$train_status <- renderMenu({
     current_prob <- predictCurrentProbabilities()
     if(!is.null(current_prob)){
       current_prob <- current_prob %>% ungroup %>% arrange(-pct)
       max_chance <- max(current_prob$pct)
-      badge_status <- if(max_chance < 50) {"success"
-      }else if(max_chance <75) {"warning"
+      badge_status <- if(max_chance < 50) {
+        "success"
+      }else if(max_chance <75) {
+          "warning"
       }else {"danger"}
+
+      # Build list of status bars
       progress_msgs <- apply(current_prob, 1, function(row) {
         taskItem(value = row[["pct"]],
                  color = getStatusColor(row[["pct"]]),
@@ -191,22 +232,20 @@ shinyServer(function(input, output, session) {
       })
     }
     else progress_msgs = list()
+
     dropdownMenu(type = "tasks", .list = progress_msgs, badgeStatus = badge_status)
   })
 
-  # Body ---------
+  # Body Output ---------
+
+  # Build dropdown to select lines
   output$line_list <- renderUI({
     line_list <- as.list(main_info$Line)
     names(line_list) <- main_info$Full.Name
-    selectInput(inputId = "trainline", label = "Train Line:", line_list, selected = 1)
+    selectInput(inputId = "trainline", label = "Train Line:", line_list, selected = 2)
   })
-  #
-  #   output$predictions <- renderDataTable({
-  #     predictCurrentProbabilities() %>%
-  #       datatable(options = list(dom = 't'), rownames = FALSE) %>%
-  #       formatStyle('p', backgroundColor = styleInterval(c(0.5,0.75), c('green','yellow','red')))
-  #     })
 
+  # Provide time since last refresh/update
   output$last_update <- renderUI({
     input$refresh
     interval <- max(as.numeric(input$interval), 5)
@@ -218,6 +257,7 @@ shinyServer(function(input, output, session) {
     )
   })
 
+  # Populate current chance of delay as a value box
   output$current_delaychance <- renderValueBox({
     current_prob <- predictCurrentProbabilities()
     if(!is.null(current_prob) & !is.null(input$trainline)){
@@ -228,6 +268,7 @@ shinyServer(function(input, output, session) {
     }else {valueBox(value = "Loading", subtitle = "", width = 12)}
   })
 
+  # Populate current conditions
   output$currentcond <- renderUI({
     current_prob <- predictCurrentProbabilities()
     current_cpt <- buildCurrentCPT()
@@ -241,6 +282,7 @@ shinyServer(function(input, output, session) {
       }else if(ttl_line < 1440){
         paste(round(ttl_line/60,digits = 2), "hours")
       }else {">24 hours"}
+
       tags$table(style = "width = 100%",
                  tags$tr(tags$td(tags$b("Last Delay:"), last_delay)),
                  tags$tr(tags$td(tags$b("Temp:"), vals$Temp_F,"°F")),
@@ -250,10 +292,14 @@ shinyServer(function(input, output, session) {
     }
     else {NULL}
   })
+
+  # Plot explaining feature contribution to prediction
   output$featureplot <- renderPlot({
     current_cpt <- buildCurrentCPT()
     if(!is.null(current_cpt) & !is.null(input$trainline)){
       cpt_line <- current_cpt[current_cpt$Line == input$trainline, ]
+
+      # Cleanup naming and enforce max/min values
       cpt_line <- cpt_line %>%
         mutate(feature = factor(feature,
                                 levels = rev(c("ttl_line_break",
@@ -269,23 +315,26 @@ shinyServer(function(input, output, session) {
                                                "Current\nVisibility",
                                                "Time of Day",
                                                "Day of\nthe Week",
-                                               "Current\nMonth"))))
+                                               "Current\nMonth")))) %>%
+        mutate(p_fold = ifelse(p_fold>2,2, ifelse(p_fold < -2, -2, p_fold)))
 
-      cpt_line %>%
-        mutate(p_fold = ifelse(p_fold>2,2, ifelse(p_fold < -2, -2, p_fold))) %>%
-        ggplot(aes(feature,p_fold, fill = p_fold)) +
-        geom_bar(stat = "identity") +
-        scale_fill_gradient2(low = "blue", mid = "black", high = "red", midpoint = 0, limits=c(-2, 2), guide = FALSE) +
-        scale_y_continuous(limits = c(-2,2),
-                           breaks = c(-2,0,2),
-                           labels = c("Decreases\nDelay\nChance","Neutral", "Increases\nDelay\nChance")) +
-        xlab("") + ylab("") +
-        coord_flip() +
-        theme_minimal() +
-        theme(axis.text=element_text(size=16))
+        # Plot
+        cpt_line %>%
+          ggplot(aes(feature,p_fold, fill = p_fold)) +
+          geom_bar(stat = "identity") +
+          scale_fill_gradient2(low = "blue", mid = "black", high = "red", midpoint = 0, limits=c(-2, 2), guide = FALSE) +
+          scale_y_continuous(limits = c(-2,2),
+                             breaks = c(-2,0,2),
+                             labels = c("Decreases\nDelay\nChance","Neutral", "Increases\nDelay\nChance")) +
+          xlab("") + ylab("") +
+          coord_flip() +
+          theme_minimal() +
+          theme(axis.text=element_text(size=16))
     }
     else{NULL}
   })
+
+  # 12 Hour forecast plot
   output$hourlyforecastplot <- renderPlot({
     hourly_prob <- predictHourlyProbabilities()
     if(!is.null(hourly_prob) & !is.null(input$trainline)){
--- railactive/ui.R
+++ railactive/ui.R
@@ -1,13 +1,5 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 library(shinydashboard)
-library(DT)
 
 header <- dashboardHeader(title = "RailActive",
                           dropdownMenuOutput(outputId = "notifications"),
@@ -19,48 +11,37 @@ body <- dashboardBody(
   tags$style(type="text/css", ".box-header .box-title{ font-size: 24px; }"),
   tags$style(type="text/css", ".main-header .logo{ font-size: 38px; font-weight: 500;}"),
 
-#   fluidRow(
-#     column(width = 12,
-#            box(collapsible = TRUE, title = "Welcome/About", width = NULL, collapsed = TRUE,
-#                p(tags$b("Railactive"), "is a dashboard designed to monitor and predict up-to-the minute short term delays for the NJTransit rail system."),
-#                p("The dashboard panel utilizes a predictive algorithm to forecast the immediate and short-term possibility of a delay occuring for NJT trains. More specifically, the predition reflects how likely it is for a given train line to experience delays of 10 minutes or more at its final destination."),
-#                h4("Control Panel"),
-#                 p("This section of the control panel allows the user to select the desired line to monitor and to chose how frequently the predictions are refreshed."),
-#                h4("Live status"),
-#                p("Provides a forecast of a delay occuring on the selected line as well as relevant information about current travel conditions."),
-#                h4("Explanation of Effect"),
-#                p("This plot summarizes how the current conditions are impacting the prediction of train delays for the chosen line."),
-#                h4("12-Hour Forecast"),
-#                p("This bottom pannel forecasts the possibility of a delay on the line for the next 12 hours.",
-#                  tags$b("Color Key:"), "green-yellow suggests a low chance of delay, orange a medium and red a high chance of delay.")
-#                )
-#     )
-#   ),
+  # Current Predictions + Controls
   fluidRow(
-    column(width = 4, box(collapsible = FALSE, width = NULL, title = "Control Panel", height = "420px",
-                          uiOutput(outputId = "line_list"),
-                          selectInput("interval", label = "Refresh interval",
-                                      choices = c(
-                                        "30 seconds" = 30,
-                                        "1 minute" = 50,
-                                        "5 minutes" = 600,
-                                        "Off" = 0),
-                                      selected = "30"),
-                          actionButton("refresh", "Refresh now"),
-                          uiOutput(outputId = "last_update")
-    )),
-    column(width = 3, box(collapsible = FALSE, width = NULL, title = "Live Status", height = "420px",
-                          valueBoxOutput("current_delaychance", width = 12),
-                          uiOutput(outputId = "currentcond")
-                          )
+    column(width = 4,
+           box(collapsible = FALSE, width = NULL, title = "Control Panel", height = "420px",
+               uiOutput(outputId = "line_list"),
+               selectInput("interval", label = "Refresh interval",
+                           choices = c(
+                             "30 seconds" = 30,
+                             "1 minute" = 50,
+                             "5 minutes" = 600,
+                             "Off" = 0),
+                           selected = "30"),
+               actionButton("refresh", "Refresh now"),
+               uiOutput(outputId = "last_update")
+           )),
+    column(width = 3,
+           box(collapsible = FALSE, width = NULL, title = "Live Status", height = "420px",
+               valueBoxOutput("current_delaychance", width = 12),
+               uiOutput(outputId = "currentcond")
+           )
     ),
-    column(width = 5, box(collapsible = FALSE, width = NULL, title = "Explanation of Effect", height = "420px",
-                          plotOutput(outputId = "featureplot", height = "350px", width = "100%")))
+    column(width = 5,
+           box(collapsible = FALSE, width = NULL, title = "Explanation of Effect", height = "420px",
+               plotOutput(outputId = "featureplot", height = "350px", width = "100%")))
   ),
+
+  # 12-hour forecast
   fluidRow(
     column(width = 12,
-    box(collapsible = TRUE, collapsed = FALSE, width = NULL, title = "12-Hour Forecast",
-        plotOutput(outputId = "hourlyforecastplot", height = "380px", width = "100%")))
+           box(collapsible = TRUE, collapsed = FALSE, width = NULL, title = "12-Hour Forecast",
+               plotOutput(outputId = "hourlyforecastplot", height = "380px", width = "100%")))
   )
 )
 
